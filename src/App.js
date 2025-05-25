d2479ced0c0068eec000ef4aeb4c5dc02af4ac3a
Minn97u <kmg09190@naver.com>
2025-05-26 00:17:33 +0900

    feat: useState 이해

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import "./App.css";
 
 function App() {
   // let post = "강남 고기 맛집";
-  const [a, b] = useState("남자 코트 추천");
-  const [logo, setLoge] = useState("Mingyu's blog");
+  const [title, setTitle] = useState([
+    "남자 코트 추천",
+    "강남 우동 맛집",
+    "파이썬독학",
+  ]);
 
   return (
     <div className="App">
@@ -14,15 +17,15 @@ function App() {
         </h1>
       </div>
       <div className="list">
-        <h3>1. 글제목</h3>
+        <h3>1. {title[0]}</h3>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h3>2. {a}</h3>
+        <h3>2. {title[1]}</h3>
         <p>2월 18일 발행</p>
       </div>
       <div className="list">
-        <h3>3. 글제목</h3>
+        <h3>3. {title[2]}</h3>
         <p>2월 19일 발행</p>
       </div>
     </div>
