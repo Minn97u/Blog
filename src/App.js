fb205872eab8c39d08037bd8fc8a845ddd92c972
Minn97u <kmg09190@naver.com>
2025-06-01 22:25:13 +0900

    feat : background 설정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 /* eslint-disable */
 
 import React from "react";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">Mingyu's Blog</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#aboutMe">About Me</Nav.Link>
+            <Nav.Link href="#gallery">Gallery / Life</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className="main-bg"></div>
+    </div>
+  );
 }
 export default App;
