8185c9a33862a8e3a894349eb5c26f075bbb2d10
Minn97u <kmg09190@naver.com>
2025-06-09 17:15:49 +0900

    feat : data 생성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 /* eslint-disable */
 
-import React from "react";
+import { useState } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import "./App.css";
+import {data} from "./data";
 
 function App() {
+  let [post] = useState();
   return (
     <div className="App">
       <Navbar bg="light" data-bs-theme="light">
@@ -21,10 +23,22 @@ function App() {
       <div className="main-bg"></div>
       <div className="container">
         <div className="row">
-          <div className="col-md-4">안녕</div>
-          <div className="col-md-4">안녕</div>
-          <div className="col-md-4">안녕</div>
-          
+          <div className="col-md-4">
+            <img src="/jsLogo.png" width="70%"></img>
+            <h5>자바 스크립트 기초 문법</h5>
+            <p>내용</p>
+            <p>{a}</p>
+          </div>
+          <div className="col-md-4">
+            <img src="/jsLogo.png" width="70%"></img>
+            <h5>자바 스크립트 응용</h5>
+            <p>내용</p>
+          </div>
+          <div className="col-md-4">
+            <img src="/jsLogo.png" width="70%"></img>
+            <h5>var,let,const에 대해 알아보자</h5>
+            <p>내용</p>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/data.js
@@ -0,0 +1,22 @@
+let data = [
+    {
+        id: 0,
+        title: "자바 스크립트 기초 문법",
+        content: "자바스크립트의 기초 문법에 대해 알아봅시다.",
+        img: "/jsLogo.png",
+    },
+    {
+        id: 1,
+        title: "자바 스크립트 응용",
+        content: "자바스크립트를 활용한 다양한 응용 방법을 배워봅시다.",
+        img: "/jsLogo.png",
+    },
+    {
+        id: 2,
+        title: "var, let, const에 대해 알아보자",
+        content: "변수 선언 방식인 var, let, const의 차이점에 대해 알아봅시다.",
+        img: "/jsLogo.png",
+    },
+]
+
+export default data;
